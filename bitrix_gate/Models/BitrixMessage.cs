3ba61574a5a7f622e328f7a6e681e30a12eeae1f
ntorgov <ntorgov@gmail.com>
2020-09-10 12:41:02 +0300

    Init

---
+++ bitrix_gate/Models/BitrixMessage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bitrix_gate.Models
+{
+	class BitrixMessage
+	{
+		/// <summary>
+		/// ID сообщения
+		/// </summary>
+		public long id { get; set; }
+
+		/// <summary>
+		/// ID чата
+		/// </summary>
+		public string chat_id { get; set; }
+
+		/// <summary>
+		/// ID автора
+		/// </summary>
+		public string author_id { get; set; }
+
+		/// <summary>
+		/// Дата
+		/// </summary>
+		public string date { get; set; }
+
+		/// <summary>
+		/// Сообщение
+		/// </summary>
+		public string text { get; set; }
+
+		/// <summary>
+		/// Флаг прочтения
+		/// </summary>
+		public bool unread { get; set; }
+	}
+}
---
+++ bitrix_gate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bitrix_gate
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
