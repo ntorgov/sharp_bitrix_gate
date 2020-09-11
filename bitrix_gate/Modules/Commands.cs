15b0350bc764ae182fe0b9a758fc3264078c8e00
ntorgov <ntorgov@gmail.com>
2020-09-11 15:13:02 +0300

    Commands

---
+++ bitrix_gate/Modules/Commands.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace bitrix_gate.Modules
+{
+	public class Commands : ModuleBase<SocketCommandContext>
+	{
+		/// <summary>
+		/// Команда определения канала
+		/// </summary>
+		/// <returns></returns>
+		[Command("detect")]
+		public async Task Detect()
+		{
+			var message = "Channel #" + Context.Channel.Id + "; ";
+			message += "Name `" + Context.Channel.Name + "`";
+			await Context.Channel.SendMessageAsync(message);
+		}
+
+		/// <summary>
+		/// Ответ на пинг
+		/// </summary>
+		/// <returns></returns>
+		[Command("ping")]
+		public async Task Ping()
+		{
+			await ReplyAsync("Pong");
+		}
+	}
+}
--- bitrix_gate/Program.cs
+++ bitrix_gate/Program.cs
@@ -1,70 +1,73 @@
-﻿using Discord.Commands;
-using Discord.WebSocket;
-using System;
-using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
 using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace bitrix_gate
 {
-    class Program
-    {
-        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+	class Program
+	{
+		static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
 
-        private DiscordSocketClient _client;
-        private CommandService _commands;
-        private IServiceProvider _services;
+		private DiscordSocketClient _client;
+		private CommandService _commands;
+		private IServiceProvider _services;
 
-        public async Task RunBotAsync()
-        {
-            _client = new DiscordSocketClient();
-            _commands = new CommandService();
+		public async Task RunBotAsync()
+		{
+			_client = new DiscordSocketClient();
+			_commands = new CommandService();
 
-            _services = new ServiceCollection()
-                .AddSingleton(_client)
-                .AddSingleton(_commands)
-                .BuildServiceProvider();
+			_services = new ServiceCollection()
+				.AddSingleton(_client)
+				.AddSingleton(_commands)
+				.BuildServiceProvider();
 
-            string token = Properties.Resources.ResourceManager.GetString(@"DISCORD_TOKEN");
+			string token = Properties.Resources.DISCORD_TOKEN;
 
-            _client.Log += _client_Log;
+			_client.Log += _client_Log;
 
-            await RegisterCommandsAsync();
+			await RegisterCommandsAsync();
 
-            await _client.LoginAsync(TokenType.Bot, token);
+			await _client.LoginAsync(TokenType.Bot, token);
 
-            await _client.StartAsync();
+			await _client.StartAsync();
 
-            await Task.Delay(-1);
+			await Task.Delay(-1);
 
-        }
+		}
 
-        private Task _client_Log(LogMessage arg)
-        {
-            Console.WriteLine(arg);
-            return Task.CompletedTask;
-        }
+		private Task _client_Log(LogMessage arg)
+		{
+			Console.WriteLine(arg);
+			return Task.CompletedTask;
+		}
 
-        public async Task RegisterCommandsAsync()
-        {
-            _client.MessageReceived += HandleCommandAsync;
-            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
-        }
+		public async Task RegisterCommandsAsync()
+		{
+			_client.MessageReceived += HandleCommandAsync;
+			await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+		}
 
-        private async Task HandleCommandAsync(SocketMessage arg)
-        {
-            var message = arg as SocketUserMessage;
-            var context = new SocketCommandContext(_client, message);
-            if (message.Author.IsBot) return;
+		private async Task HandleCommandAsync(SocketMessage arg)
+		{
+			var message = arg as SocketUserMessage;
+			var context = new SocketCommandContext(_client, message);
+			if (message.Author.IsBot) { return; }
 
-            int argPos = 0;
-            if (message.HasStringPrefix("!", ref argPos))
-            {
-                var result = await _commands.ExecuteAsync(context, argPos, _services);
-                if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
-                if (result.Error.Equals(CommandError.UnmetPrecondition)) await message.Channel.SendMessageAsync(result.ErrorReason);
-            }
-        }
-    }
+			int argPos = 0;
+			if (message.HasStringPrefix("!", ref argPos))
+			{
+				var result = await _commands.ExecuteAsync(context, argPos, _services);
+				if (!result.IsSuccess) { Console.WriteLine(result.ErrorReason); }
+				if (result.Error.Equals(CommandError.UnmetPrecondition)) { await message.Channel.SendMessageAsync(result.ErrorReason); }
+			}
+		}
+	}
 }
